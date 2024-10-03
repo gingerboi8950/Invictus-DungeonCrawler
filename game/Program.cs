51ca1903c3e934c4d9c97e790550204a793b1e7a
Rooted1 <ruthobey@gmail.com>
2024-10-03 14:09:31 -0500

    define fooditem class with name and health attributes

---
+++ game/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class FoodItem
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+
+    public FoodItem(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FoodItem apple = new FoodItem("Apple", 10);
+        Console.WriteLine($"Food: {apple.Name}, Health: {apple.Health}");
+    }
+}
